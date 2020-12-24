d01a366069105e8349b06a723a266c2af600f315
jang <dktkqyd99@gmail.com>
2020-12-24 10:22:15 +0900

    [Server]fix

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const sign = require('./routes/sign');
+const copy = require('./routes/copy');
+const user = require('./routes/user')
 
 const app = express();
+
 app.use(cors({
     origin : true,
     methods : ["GET","POST","OPTIONS"],
@@ -19,8 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/sign', sign);
+app.use('/copy', copy);
+app.use('/user', user);
 
 app.listen(8080);
 
---
+++ server/routes/sign.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const controllers = require('./controllers/sign');
+
+router.post('/signin', controllers.signInController)
+router.post('/signup', controllers.signUpController)
+router.post('/logout', controllers.logoutController)
+
+
+module.exports = router;
